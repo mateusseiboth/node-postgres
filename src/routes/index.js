0410c9970c0b2496b93c0c8e598d38f720309c5e
mateusseiboth <mateusseiboth@gmail.com>
2023-04-11 17:13:21 -0400

    Importada rotas de client

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const userRouter = require('./user');
 //const ticketRouter = require('./Ticket/ticket');
 const vagaRouter = require('./vaga');
 const tipoRouter = require('./tipo');
+const clientRouter = require('./client');
 
 const router = Router();
 
@@ -12,6 +13,7 @@ router.use('/user', userRouter);
 //router.use('/ticket', ticketRouter);
 router.use('/tipo', tipoRouter);
 router.use('/vaga', vagaRouter);
+router.use('/client', clientRouter)
 
 
 module.exports = router;
