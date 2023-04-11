fa44fcf1585308a657d8b512524b3ccf2164c642
mateusseiboth <mateusseiboth@gmail.com>
2023-04-11 17:13:14 -0400

    Adicionadas rotas de client

---
+++ src/routes/client.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const clientController = require('../controllers/client');
+const { verifyToken } = require('../middlewares/auth');
+
+router.post('/new', verifyToken, clientController.createClient, (req, res) => {
+  res.status(201).json({message: "Client criado"})
+});
+
+router.delete('/:id', verifyToken, clientController.deleteClient, (req, res) => {
+  res.status(200).json({ message: 'Client excluído com sucesso' });
+});
+
+router.put('/update', verifyToken, clientController.updateClient, (req, res) => {
+    res.status(200).json({message: 'Client atualizado com sucesso'})
+});
+
+module.exports = router;
