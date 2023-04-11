231335ffa1dd938dbaddd09e1e65e671c2c24603
mateusseiboth <mateusseiboth@gmail.com>
2023-04-11 17:37:54 -0400

    Adição de funções do controller

--- src/controllers/ticket.js
+++ src/controllers/ticket.js
@@ -2,7 +2,7 @@ const Ticket = require('../models/ticket');
 
 const createTicket = async (req, res, next) => {
   try {
-    const novoTicket = await Ticket.insertTicket();
+    const novoTicket = await Ticket.insertTicket(req, res);
     next();
   } catch (error) {
     console.error(error);
@@ -10,6 +10,16 @@ const createTicket = async (req, res, next) => {
   }
 };
 
+const encerrarTicket = async (req, res) => {
+    try {
+      const { id } = req.params;
+      await Ticket.encerraTicket(id);
+      res.status(200).json({ message: 'ok' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Erro ao encerrar Ticket' });
+    }
+  };
 
 
-module.exports = { createTicket };
+module.exports = { createTicket, encerrarTicket };
