cccdd3e45faad2b70bd20aced8d3932332d7afd5
mateusseiboth <mateusseiboth@gmail.com>
2023-04-11 16:44:37 -0400

    Criado controlador do carro

---
+++ src/controllers/cars.js
@@ -0,0 +1,35 @@
+const Car = require('../models/cars');
+
+const createCar = async (req, res, next) => {
+  try {
+    const car = req.body;
+    const novoCar = await Car.insertCar(car);
+    next();
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+const updateCar = async (req, res) => {
+  try {
+    const car = req.body;
+    await Car.updateCar(car);
+    res.status(200).json({ message: 'ok' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao atualizar Car' });
+  }
+};
+
+const deleteCar = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Car.deleteCar(id);
+    res.status(200).json({ message: 'ok' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erro ao deletar Car' });
+  }
+};
+
+module.exports = { createCar, deleteCar, updateCar };
