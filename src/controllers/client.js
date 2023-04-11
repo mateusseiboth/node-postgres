4051957e6faf9af16119fbae785707f369b28570
mateusseiboth <mateusseiboth@gmail.com>
2023-04-11 17:12:55 -0400

    Atualizada funcoes de clientController

--- src/controllers/client.js
+++ src/controllers/client.js
@@ -1,4 +1,4 @@
-const Client = require('../models/cars');
+const Client = require('../models/client');
 
 const createClient = async (req, res, next) => {
   try {
@@ -10,6 +10,28 @@ const createClient = async (req, res, next) => {
   }
 };
 
+const updateClient = async (req, res) => {
+    try {
+      const client = req.body;
+      await Client.updateClient(client);
+      res.status(200).json({ message: 'ok' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Erro ao atualizar client' });
+    }
+  };
+  
+  const deleteClient = async (req, res) => {
+    try {
+      const { id } = req.params;
+      await Client.deleteClient(id);
+      res.status(200).json({ message: 'ok' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Erro ao deletar cliente' });
+    }
+  };
 
 
-module.exports = { createClient };
+
+module.exports = { createClient, updateClient, deleteClient };
