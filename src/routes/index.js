078d5b3f8a4648c7554e8dfd09804a63700812c7
mateusseiboth <mateusseiboth@gmail.com>
2023-04-11 16:45:05 -0400

    Importada rotas do carro na index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-//const carRouter = require('./Cars/cars');
+const carRouter = require('./cars');
 const userRouter = require('./user');
 //const ticketRouter = require('./Ticket/ticket');
 const vagaRouter = require('./vaga');
@@ -7,7 +7,7 @@ const tipoRouter = require('./tipo');
 
 const router = Router();
 
-//router.use('/car', carRouter);
+router.use('/car', carRouter);
 router.use('/user', userRouter);
 //router.use('/ticket', ticketRouter);
 router.use('/tipo', tipoRouter);
