d8f920dfadb28bf775060cb666ddf5c879f26c18
mateusseiboth <mateusseiboth@gmail.com>
2023-04-11 17:38:13 -0400

    Importada rotas do ticket

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 const carRouter = require('./cars');
 const userRouter = require('./user');
-//const ticketRouter = require('./Ticket/ticket');
+const ticketRouter = require('./ticket');
 const vagaRouter = require('./vaga');
 const tipoRouter = require('./tipo');
 const clientRouter = require('./client');
@@ -10,7 +10,7 @@ const router = Router();
 
 router.use('/car', carRouter);
 router.use('/user', userRouter);
-//router.use('/ticket', ticketRouter);
+router.use('/ticket', ticketRouter);
 router.use('/tipo', tipoRouter);
 router.use('/vaga', vagaRouter);
 router.use('/client', clientRouter)
